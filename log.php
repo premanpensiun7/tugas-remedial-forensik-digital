7729646a0e53842fcebf1a72518c023fd62e22b5
programer <85097802+Eriyulianhidayat@users.noreply.github.com>
2022-08-10 11:30:59 +0700

    Add files via upload

---
+++ log.php
@@ -0,0 +1,14 @@
+<?php 
+function debugLog($o) {
+ $file_bulan = 'log/' . 'log' . date("Y-m") . '.log';
+ ob_start();
+ var_dump($o);
+ $c = ob_get_contents();
+ ob_end_clean();
+ $flog=fopen($file_log, "a+");
+ fputs($flog,"$c\n");
+ fclose($flog);
+}
+?>
+
+//eri yulian hidayat 1955201005
